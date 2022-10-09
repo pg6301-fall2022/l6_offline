29c7d34914597bf687ef2b41d8b6ff1fb5a80240
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-10-09 14:19:08 +0200

    Step 2 - basic server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/login", (req, res, next) => {
+   res.json({username: "Testsson"});
+});
+
+app.post("/login", (req, res, next) =>{
+   res.sendStatus(401);
+});
+
+const server = app.listen(process.env.PORT || 3000,
+    () => {
+   console.log(`Server started at http://localhost:${server.address().port}`)
+    });
